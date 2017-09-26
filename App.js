08bd32280f3cfc3d4ba9259140c20d009428a805
Jesse Liston <jesselis@cs.washington.edu>
2017-09-25 22:31:56 -0700

    trying to add bittrex api

--- App.js
+++ App.js
@@ -22,16 +22,53 @@ export default class App extends React.Component {
     const beans = [ this.getBeanImage(0) ];
 
     for (var i = 1; i < this.state.numBeans; i += 1) {
-      beans.push( this.getBeanImage(i));
+      beans.push( this.getBeanImage(i) );
     }
 
     return (
-      <TouchableOpacity style={styles.container} onPress={this._onPressButton.bind(this)}>
-        <View style={styles.button}>
-          {beans}
+      <View style={styles.container} >
+        {/*
+        <TouchableOpacity onPress={this._onPressButton.bind(this)}>
+          <View style={styles.button}>
+            {beans}
+          </View>
+        </TouchableOpacity>
+        */}
+        <View>
+            <BitbeanMarket/>
         </View>
-      </TouchableOpacity>
+      </View>
     );
+  }
+}
+
+class BitbeanMarket extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {items:[]}
+  }
+
+  _onPressButton() {
+  }
+
+  componentDidMount() {
+    fetch('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-bitb').then(result=> {
+      this.setState({items: [result.json()] })}
+    ).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  render() {
+    return(
+      <TouchableOpacity onPress={this._onPressButton.bind(this)}>
+        <View>
+          <Text>Items:</Text>
+          <Text>{JSON.stringify(this.state.items)} </Text>
+        </View>
+      </TouchableOpacity>)
+      
   }
 }
 
@@ -47,7 +84,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginBottom: 30,
-    width: 260,
+    width: 265,
     alignItems: 'center',
     backgroundColor: '#FFFFFF'
   },
